aacefa55be6f385d6a30a792f1119ece66fa6a34
bandura <banduraweb@ukr.net>
2019-12-06 04:47:05 +0200

    Updates

---
+++ precache-manifest.ecc03d4313200955a900f20a629ede68.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8183857da88e26b2b1339a27ae6527d4",
+    "url": "/react_tabs/index.html"
+  },
+  {
+    "revision": "477d3cfdba83b1e0f343",
+    "url": "/react_tabs/static/js/2.345f8e30.chunk.js"
+  },
+  {
+    "revision": "e3051424710ae2580a6c",
+    "url": "/react_tabs/static/js/main.1d59ad03.chunk.js"
+  },
+  {
+    "revision": "010ecae327fc4474afd9",
+    "url": "/react_tabs/static/js/runtime~main.310f5775.js"
+  }
+]);
